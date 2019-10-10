7e5ed626b265832f9c3e929b72b63293608159c5
Olga Ivanova <ole4ka_0687@mail.ru>
2019-10-10 10:21:33 +0300

    phone-catalogue was created

---
+++ js/phone-page.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class PhonePage {
+	constructor(options) {
+		this._el = options.el;
+
+		this._catalogue = new PhoneCatalogue({
+			el: this._el.querySelector('[data-component="phone-catalogue"]')
+		});
+	}
+
+	
+}
