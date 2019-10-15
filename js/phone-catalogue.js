131427b1b15091d444817b9d0f98c686e46ec708
Olga Ivanova <ole4ka_0687@mail.ru>
2019-10-15 11:41:30 +0300

    lodash template was added

--- js/phone-catalogue.js
+++ js/phone-catalogue.js
@@ -3,13 +3,17 @@
 class PhoneCatalogue {
 	constructor(options) {
 		this._el = options.el;
+		this._phones = options.phones;
 
 		this._render();
 	}
 
 	_render() {
-		const template = document.querySelector('#phone-catalogue-template').innerHTML
+		const template = document.querySelector('#phone-catalogue-template').innerHTML;
+		const templateFunction = _.template(template);
 
-		this._el.innerHTML = template;
+		this._el.innerHTML = templateFunction({
+			phones: this._phones,
+		});
 	}
 }
