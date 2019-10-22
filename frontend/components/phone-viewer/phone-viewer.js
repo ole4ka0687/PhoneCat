42cb251b554900a628f55ac70219d4318cfaf672
Olga Ivanova <ole4ka_0687@mail.ru>
2019-10-22 09:56:04 +0300

    phone viewer was created

---
+++ frontend/components/phone-viewer/phone-viewer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import compiledTemplate from './template.hbs';
+
+export default class PhoneViewer {
+	constructor(options) {
+		this._el = options.el;
+	}
+
+	setPhone(phone) {
+		this._phone = phone;
+		this._render();
+	}
+
+	_render() {
+
+		this._el.innerHTML = compiledTemplate({
+			phone: this._phone,
+		});
+	}
+}
